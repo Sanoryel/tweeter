7c8dd6abd4536438e0483c4f5314818da983de5f
Sanoryel <lucas.leyronas@orange.fr>
2024-03-26 17:55:33 +0100

    Add routes for tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'tweets/index'
-  get 'tweets/create'
-  get 'tweets/destroy'
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
-
+  
+  resources :tweets, except: [:edit, :update]
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root to: 'tweet#index'
 end
