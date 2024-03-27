c636fb05265d47dba6b25e7e3f4e00da1f18545d
Sanoryel <lucas.leyronas@orange.fr>
2024-03-27 22:13:28 +0100

    Controller tweet continue

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
 class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
   end
 
   def destroy
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+
 end
